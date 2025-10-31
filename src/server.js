058719bbb305ad2081bccb73c75eadb46f970879
Frigofel1110 <130562156+Frigofel1110@users.noreply.github.com>
2025-10-31 21:54:20 +0100

    config pour mettre en prod

--- src/server.js
+++ src/server.js
@@ -7,14 +7,30 @@ const authRoutes = require("./routes/auth");
 const generatorRoutes = require("./routes/generator");
 const ocrRoutes = require("./routes/ocr");
 
-
 var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//CORS
+const allowedOrigins = [
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 
 //MIDLEWARES
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173", //url frontend
-    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Pas autorisé pars CORS"));
+      }
+    },
+    credentials: "true",
   })
 );
 
@@ -28,8 +44,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false, //mettre true en prod
-      sameSite: 'lax',
+      secure: process.env.NODE_ENV === "production" ? "true" : "false",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 3600000, // 1heure
     },
   })
@@ -38,7 +54,11 @@ app.use(
 app.use("/auth", authRoutes);
 app.use("/api/generator", generatorRoutes);
 app.use("/api/ocr", ocrRoutes);
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 //START
 app.listen(3000, () => {
-  console.log("Backend sur http://127.0.0.1:3000");
+  console.log(`Backend sur le port: ${PORT}`);
 });
